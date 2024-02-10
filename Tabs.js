9d609e5187b117103410d55c3d863d8bd1c6f651
narasimhanaidu <talarinarasimhanaidu1@gmail.com>
2024-02-10 21:23:20 +0530

    small changes

--- Tabs.js
+++ Tabs.js
@@ -1,40 +1,38 @@
 let AboutTab = document.getElementById("aboutTab");
 let TimeToVisitTab = document.getElementById("timeToVisitTab");
 let AttractionsTab = document.getElementById("attractionsTab");
+let BgColorChangeaboutButton = document.getElementById("aboutButton");
+let BgColorChangetimeToVisitButton = document.getElementById("timeToVisitButton");
+let BgColorChangeattractionsButton = document.getElementById("attractionsButton");
 TimeToVisitTab.classList.add("d-none");
 AttractionsTab.classList.add("d-none");
-function OnclickaboutButton(){
+
+function OnclickaboutButton() {
     AboutTab.classList.remove("d-none")
     TimeToVisitTab.classList.add("d-none");
     AttractionsTab.classList.add("d-none");
 
-    AboutTab.classList.add("selected-button");
-    TimeToVisitTab.classList.add("selected-button");
-    AttractionsTab.classList.remove("selected-button");
+    BgColorChangeaboutButton.classList.add("selected-button");
+    BgColorChangetimeToVisitButton.classList.remove("selected-button");
+    BgColorChangeattractionsButton.classList.remove("selected-button");
 }
 
-function OnclicktimeToVisitButton(){
+function OnclicktimeToVisitButton() {
     AboutTab.classList.add("d-none")
     TimeToVisitTab.classList.remove("d-none");
     AttractionsTab.classList.add("d-none");
 
-    AboutTab.classList.remove("selected-button");
-    TimeToVisitTab.classList.add("selected-button");
-    AttractionsTab.classList.remove("selected-button");
+    BgColorChangeaboutButton.classList.remove("selected-button");
+    BgColorChangetimeToVisitButton.classList.add("selected-button");
+    BgColorChangeattractionsButton.classList.remove("selected-button");
 }
 
-function OnclickattractionButton(){
+function OnclickattractionButton() {
     AboutTab.classList.add("d-none")
     TimeToVisitTab.classList.add("d-none");
     AttractionsTab.classList.remove("d-none");
 
-    AboutTab.classList.remove("selected-button");
-    TimeToVisitTab.classList.remove("selected-button");
-    AttractionsTab.classList.add("selected-button");
+    BgColorChangeaboutButton.classList.remove("selected-button");
+    BgColorChangetimeToVisitButton.classList.remove("selected-button");
+    BgColorChangeattractionsButton.classList.add("selected-button");
 }
-
-
-
-
-
-
